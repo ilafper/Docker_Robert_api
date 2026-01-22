01f91da638f3a060b910b8a2d87e875100eda5b2
ilafper <ivanaalfaya@gmail.com>
2026-01-22 19:29:29 +0100

    prueba13

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ async function iniciarServidor() {
 
 
         app.get("/", async (req, res) => {
-            res.json({message:'prueba webhook12'});
+            res.json({message:'prueba webhook13'});
         });
 
 
