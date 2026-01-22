fa4268537ee86c52f49a8a68f83ecdba5702ccea
ilafper <ivanaalfaya@gmail.com>
2026-01-22 19:09:42 +0100

    try webhook8

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ async function iniciarServidor() {
 
 
         app.get("/", async (req, res) => {
-            res.json({message:'prueba webhook6'});
+            res.json({message:'prueba webhook8'});
         });
 
 
